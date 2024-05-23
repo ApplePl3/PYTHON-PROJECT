769ed532ace7f41c835cb85700742a753dd890ce
ApplePl3 <104432161+ApplePl3@users.noreply.github.com>
2024-05-24 00:42:19 +0700

    Add files via upload

---
+++ Camera.py
@@ -0,0 +1,31 @@
+import pygame as pg
+
+from Const import *
+
+# Khởi tạo camera
+class Camera(object):
+    def __init__(self, width, height):
+        self.rect = pg.Rect(0, 0, width, height)
+        self.complex_camera(self.rect)
+
+    def complex_camera(self, target_rect):
+        x, y = target_rect.x, target_rect.y
+        width, height = self.rect.width, self.rect.height
+        x, y = (-x + WINDOW_W / 2 - target_rect.width / 2), (-y + WINDOW_H / 2 - target_rect.height)
+
+        x = min(0, x)
+        x = max(-(self.rect.width - WINDOW_W), x)
+        y = WINDOW_H - self.rect.h
+
+        return pg.Rect(x, y, width, height)
+
+# Áp dụng camera lên mục tiêu được chọn
+    def apply(self, target):
+        return target.rect.x + self.rect.x, target.rect.y
+#Cập nhật vị trí camera dựa trên mục tiêu
+    def update(self, target):
+        self.rect = self.complex_camera(target)
+
+# Đặt lại vị trí của camera về gốc tọa độ
+    def reset(self):
+        self.rect = pg.Rect(0, 0, self.rect.w, self.rect.h)
